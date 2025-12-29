1d249b7a3a3e0f08358325b380d6c2b1e17d21e9
CDGames1 <63894306+CDGames1@users.noreply.github.com>
2025-12-29 16:27:28 +1300

    29.12.2025
    Added new features like, the Coins to the level scene, coin counter variable into the Globals so it can be called and use a Area3D to detect when a coin is touched by the player it will then add +1 to the counter of it. as well as animation of the coin so it now rotates.

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,1 +1,11 @@
 extends Area3D
+
+func _ready():
+	pass
+
+func _process(delta):
+	print(Global.coin_counter)
+func _on_body_entered(body):
+	if body.name == "Player":
+		Global.coin_counter+=1
+		print("Coin Collected")
