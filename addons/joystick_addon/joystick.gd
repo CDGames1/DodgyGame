4c8ac564aee99479f3f246278a8dc5c0c952c3c4
CDGames1 <63894306+CDGames1@users.noreply.github.com>
2026-01-19 13:00:41 +1300

    19.01.2026
    Today was me implementing some of the Feedback from Sprint 1 into Sprint 2, now with the Joystick Stickness was a major issue, that has now been fixed. i completly redone the Joystick Code as well as fixed the Enemy Movement Skipping a Frame Bug which was discovered.

--- addons/joystick_addon/joystick.gd
+++ addons/joystick_addon/joystick.gd
@@ -15,72 +15,62 @@ var radius = 40
 func _draw() -> void:
 	var center = Vector2(0, 0)
 	var draw_radius = 100
-	var start_angle = 0.0 
-	var end_angle = 360 
-	var color = Color.POWDER_BLUE # Red color
+	var start_angle = 0.0
+	var end_angle = TAU
+	var color = Color.POWDER_BLUE
 	var width = 20
-	draw_arc(center, draw_radius, start_angle, end_angle,120, color, width,true)
-	draw_circle(touch.position,60,Color.SKY_BLUE)
-	draw_arc(touch.position, 60, start_angle, end_angle,120, Color.STEEL_BLUE, width,true)
+	draw_arc(center, draw_radius, start_angle, end_angle, 120, color, width, true)
+	draw_circle(touch.position, 60, Color.SKY_BLUE)
+	draw_arc(touch.position, 60, start_angle, end_angle, 120, Color.STEEL_BLUE, width, true)
 
-func _process(_delta: float) -> void:
+func _input(event):
+	if event is InputEventScreenTouch:
+		if event.pressed:
+			_process_touch(event.position)
+		else:
+			reset_dir()
+			touch.global_position = init_pos.global_position
+			queue_redraw()
+	elif event is InputEventScreenDrag:
+		_process_touch(event.position)
+
+func _process_touch(touch_pos: Vector2):
 	queue_redraw()
-	var mouse_pos = get_global_mouse_position()
-	var touch_pos = init_pos.global_transform.origin
-	var distance = touch_pos.distance_to(mouse_pos)
-	var mouse_dir = (mouse_pos-touch_pos).normalized()
+	var centre = init_pos.global_position
+	var distance = centre.distance_to(touch_pos)
+	var dir = touch_pos - centre
 	if distance > radius:
-		mouse_pos = touch_pos + (mouse_dir * radius)
-	touch.global_transform.origin = mouse_pos
-	if touch.position != init_pos.position:
-		var angle = init_pos.get_angle_to(mouse_pos)
+		touch_pos = centre + dir.normalized() * radius
+	touch.global_position = touch_pos
+
+	if touch.global_position != init_pos.global_position:
+		var angle = (touch_pos - centre).angle()
+		reset_dir()
 		if angle >= -0.5 and angle <= 0.5:
-			#print("right")
-			reset_dir()
 			Input.action_press(right_input)
-		elif angle >= 0.5 and angle <= 1.0:
-			#print("down-right")
-			reset_dir()
+		elif angle > 0.5 and angle <= 1.0:
 			Input.action_press(down_input)
 			Input.action_press(right_input)
-		elif angle >= 1.0 and angle <= 2.1:
-			#print("down")
-			reset_dir()
+		elif angle > 1.0 and angle <= 2.1:
 			Input.action_press(down_input)
-		elif angle >= 2.1 and angle <= 2.7:
-			#print("down-left")
-			reset_dir()
+		elif angle > 2.1 and angle <= 2.7:
 			Input.action_press(down_input)
 			Input.action_press(left_input)
-		elif angle >= 2.7 and angle <= 3.2:
-			#print("left")
-			reset_dir()
+		elif angle > 2.7 and angle <= 3.2:
 			Input.action_press(left_input)
 		elif angle >= -3.2 and angle <= -2.7:
-			#print("left")
-			reset_dir()
 			Input.action_press(left_input)
-		elif angle >= -2.7 and angle <= -2.1:
-			#print("up-left")
-			reset_dir()
+		elif angle > -2.7 and angle <= -2.1:
 			Input.action_press(up_input)
 			Input.action_press(left_input)
-		elif angle >= -2.1 and angle <= -1.0:
-			#print("up")
-			reset_dir()
+		elif angle > -2.1 and angle <= -1.0:
 			Input.action_press(up_input)
-		elif angle >= -1.0 and angle <= -0.5:
-			#print("top-right")
-			reset_dir()
+		elif angle > -1.0 and angle < -0.5:
 			Input.action_press(up_input)
 			Input.action_press(right_input)
 
-#Reset inputs
 func reset_dir():
-	if up_input and down_input and left_input and right_input == "":
-		return
-	elif up_input == "move_up" and down_input == "move_down" and left_input == "move_left" and right_input == "move_right":
-		Input.action_release(up_input)
-		Input.action_release(down_input)
-		Input.action_release(left_input)
-		Input.action_release(right_input)
+	Input.action_release(up_input)
+	Input.action_release(down_input)
+	Input.action_release(left_input)
+	Input.action_release(right_input)
